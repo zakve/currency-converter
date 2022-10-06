4c9adf8c72814e015a393b9d2381d21f151f051a
zakve <zaklas.martin@gmail.com>
2022-10-06 18:27:30 +0200

    Fix default test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders currency converter', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerText = screen.getByText(/currency converter/i);
+  expect(headerText).toBeInTheDocument();
 });
