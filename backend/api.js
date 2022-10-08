d6c6678e23b1adce682295bd1ab77fa127a68b4a
zakve <zaklas.martin@gmail.com>
2022-10-08 10:31:45 +0200

    Add backend status codes

--- backend/api.js
+++ backend/api.js
@@ -10,20 +10,16 @@ const exchangeRateUrl = "https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-
 
 app.get("/exchange-rate", (req, res) => {
     request(exchangeRateUrl, (error, response, body) => {
-        if (error) {
-            return res.send('Unable to connect to National Bank service!')
-        }
-
         if (response.statusCode === 200) {
             return res.send(body)
         }
 
-        res.send('Something went wrong!')
+        res.status(500).send('Unable to connect to National Bank service!')
     });
 })
 
 app.get('*', (req, res) => {
-    res.send('404 page')
+    res.status(404).send('404 page')
 })
 
 app.listen(port, () => {
